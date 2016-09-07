a11ee277255afb69fa21398f7b652fb0ffbb5710
Kathryn Hansen <katie112892@aol.com>
2016-09-07 15:14:41 -0500

    adds react routing and routing end points for user's page (#12)

--- client/main.js
+++ client/main.js
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App';
-
-ReactDOM.render(
-  <App />,
+import { Router, Route, Link} from 'react-router';
+import UsersPage from './components/UsersPage';
+ReactDOM.render((
+  <Router>
+    <Route path='/' component={App}>
+    </Route>
+    <Route path='/users/:userid' component={UsersPage}>
+    </Route>
+  </Router>
+  ),
   document.getElementById('app')
 );
